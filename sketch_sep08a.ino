9a66b924d28ea1d0dfc6a64e85e5401db74caf96
qasx920624 <70551884+qasx920624@users.noreply.github.com>
2020-09-08 14:23:22 +0800

    Add files via upload

---
+++ sketch_sep08a.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  for(int i=2;i<6;i++)
+    pinMode(i,OUTPUT);
+   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=2;i<6;i++)
+  digitalWrite(i,HIGH);
+  delay(300);
+
+  for(int i=2;i<6;i++)
+  digitalWrite(i,LOW); 
+  delay(300);
+ 
+}
